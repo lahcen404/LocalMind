d0103aa582ea30ef143ab46d86fcb0e29f2fb976
lahcen404 <lahcen.maskour2003@gmail.com>
2026-01-28 16:05:32 +0100

    create register and login controller

--- src/app/Http/Controllers/AuthController.php
+++ src/app/Http/Controllers/AuthController.php
@@ -2,15 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Enums\UserRole;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
 
 class AuthController extends Controller
 {
     public function showRegister(){
         return view('auth.register');
+    }
+
+    public function showLogin(){
+        return view('auth.login');
     }
 
     public function register(Request $request){
@@ -27,10 +33,35 @@ class AuthController extends Controller
         'password' => Hash::make($validated['password'])
     ]);
 
-    Auth::login($user);
+    // Auth::login($user);
 
     return redirect()->route('login');
     }
 
-   
+    // login 
+
+    public function login(Request $request){
+
+        $credentials =$request->validate([
+            'email'=> 'required|email',
+            'password'=>'required'
+        ]);
+
+        if(Auth::attempt($credentials,$request->remember)){
+
+            $request->session()->regenerate();
+
+            $user = Auth::user();
+
+        if($user->role === UserRole::ADMIN){
+            return redirect()->route('admin.dashboard');
+        }
+            return redirect()->route('dashboard');
+        }
+        
+        throw ValidationException::withMessages([
+            'email' => 'Email or Password incorrect try again'
+        ]);
+    
+    }
 }
--- src/routes/web.php
+++ src/routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {return view('welcome');});
 
+Route::get('/dashbaord' , function(){return view('auth.login');})->name('dashboard');
 Route::get('/login' , function(){return view('auth.login');})->name('login');
 Route::get('/register', function(){return view('auth.register');})->name('register');
 Route::post('/register', [AuthController::class, 'register'])->name('register.submit');
