2409354c2f875a6dac7657ac2e9e03fcf6c3e064
lahcen404 <lahcen.maskour2003@gmail.com>
2026-01-28 11:11:26 +0100

    create model of questions

--- src/app/Models/Question.php
+++ src/app/Models/Question.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Question extends Model
 {
-    //
+    protected $fillable = [
+        'title','content','location','user_id'
+    ];
+
+    // relationship
+
+    public function user(){
+
+        return $this->belongsTo(User::class);
+    }
 }
