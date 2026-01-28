2f491ea5899623335f2eb3de88197088394044ba
lahcen404 <lahcen.maskour2003@gmail.com>
2026-01-28 12:21:50 +0100

    create views/layouts (header - footer)

---
+++ src/resources/views/partials/footer.blade.php
@@ -0,0 +1,11 @@
+<footer class="mt-20 py-10 border-t border-zinc-800 bg-zinc-950">
+    <div class="max-w-7xl mx-auto px-4 text-center">
+        <div class="flex justify-center space-x-6 mb-6">
+            <a href="#" class="text-zinc-500 hover:text-indigo-400"><i class="fa-brands fa-github"></i></a>
+            <a href="#" class="text-zinc-500 hover:text-indigo-400"><i class="fa-brands fa-twitter"></i></a>
+        </div>
+        <p class="text-zinc-600 text-xs tracking-widest uppercase">
+            &copy; {{ date('Y') }} LocalMind Network. Built for Community Intelligence.
+        </p>
+    </div>
+</footer>
